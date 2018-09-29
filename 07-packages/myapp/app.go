bf1da2b491a1719be22bdf7f41e3a46f3f395eb7
Rajeev Kumar Singh <rajeevs@flock.co>
2018-09-29 12:12:03 +0530

    cleanup

--- 07-packages/myapp/app.go
+++ 07-packages/myapp/app.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 	"github.com/callicoder/golang-tutorials/07-packages/numbers"
 	"github.com/callicoder/golang-tutorials/07-packages/strings"	
-	"github.com/callicoder/golang-tutorials/07-packages/strings/greetings" // Importing a nested package
+	"github.com/callicoder/golang-tutorials/07-packages/strings/greeting" // Importing a nested package
 	str "strings"	// Package Alias
 )
 
 func main() {
 	fmt.Println(numbers.IsPrime(19))
 
-	fmt.Println(greetings.WelcomeText)
+	fmt.Println(greeting.WelcomeText)
 
 	fmt.Println(strings.Reverse("callicoder"))
 
