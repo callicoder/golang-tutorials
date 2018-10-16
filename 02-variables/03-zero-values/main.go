166cbb954b7ad54d9f82caf36f4fde6464ddd872
Rajeev Kumar Singh <callicoder@gmail.com>
2018-10-16 22:46:43 +0530

    cleanup

--- 02-variables/03-zero-values/main.go
+++ 02-variables/03-zero-values/main.go
@@ -5,9 +5,9 @@ import "fmt"
 func main() {
 	var (
 		firstName, lastName string
-		age int
-		salary float64
-		isConfirmed bool
+		age                 int
+		salary              float64
+		isConfirmed         bool
 	)
 
 	fmt.Printf("firstName: %s, lastName: %s, age: %d, salary: %f, isConfirmed: %t\n",
