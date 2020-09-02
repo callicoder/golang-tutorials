79cd3783924349fd4fbccb56298d2f048bb518e5
justin-jones <52481185+justinemanueljones@users.noreply.github.com>
2020-09-02 01:25:09 -0400

    var commit

--- 01-hello-world/main.go
+++ 01-hello-world/main.go
@@ -1,8 +1,4 @@
 // My first Program
 package main
 
-import "fmt"
-
-func main() {
-	fmt.Println("Hello, World")
-}
+import "fmt
--- 02-variables/01-variable-declaration/main.go
+++ 02-variables/01-variable-declaration/main.go
@@ -9,24 +9,20 @@ func main() {
 	var myFloat float64 = 45.12
 	fmt.Println(myStr, myInt, myFloat)
 
-
 	//================================
-
 
 	// Multiple Declarations
 	var (
-		employeeId int = 5
-		firstName, lastName string = "Satoshi", "Nakamoto"
+		employeeId          int    = 5
+		firstName, lastName string = "Justin", "Jones"
 	)
 	fmt.Println(employeeId, firstName, lastName)
 
-
 	//================================
-
 
 	// Short variable declaration syntax
 	name := "Rajeev Singh"
-	age, salary, isProgrammer := 35, 50000.0, true
+	age, salary, isProgrammer := 55, 50000.0, true
 
 	fmt.Println(name, age, salary, isProgrammer)
 }
